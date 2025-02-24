b8281346df51fa04f0430e3b0c55ddcc61809dad
beatrizristau <br0066@mix.wvu.edu>
2025-02-23 20:57:46 -0700

    [feat] created controller layer + couple of rest APIs

---
+++ src/main/java/com/bbomanso/em/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.bbomanso.em.controller;
+
+import com.bbomanso.em.dto.EmployeeDto;
+import com.bbomanso.em.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping(path = "/create", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto request) {
+        log.info("Request arrived to CREATE employee");
+        EmployeeDto response = employeeService.createEmployee(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/{employeeId}", produces = "application/json")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long employeeId) {
+        log.info("Request arrived to GET employee by id: {}", employeeId);
+        EmployeeDto response = employeeService.getEmployeeById(employeeId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "", produces = "application/json")
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        log.info("Request arrived to GET all employees");
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+}

dc8ad0c1e391000a942d65ecd6eabd09d6b17c96
beatrizristau <br0066@mix.wvu.edu>
2025-03-16 14:56:28 -0700

    [feat] creation of JWT Authentication filter

---
+++ em-backend/src/main/java/com/bbomanso/em/security/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.bbomanso.em.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        /* get JWT token from HTTP request */
+        String jwtToken = getTokenFromRequest(request);
+
+        /* validate the JWT token */
+        if (StringUtils.hasText(jwtToken) && jwtTokenProvider.validateToken(jwtToken)) {
+            /* get the username from the JWT token */
+            String username = jwtTokenProvider.getUsernameFromToken(jwtToken);
+
+            /* get the user details from the username */
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            /* create an authenticationToken token for the user */
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            /* set details of the authenticationToken token */
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            /* set the authenticationToken token in the security context */
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
